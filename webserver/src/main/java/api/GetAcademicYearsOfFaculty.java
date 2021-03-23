70c81bba4d435dcda5755811b781e44938d9c126
Hola <Hola@Hola>
2021-03-23 15:51:55 +0700

    server: create test files GetAcademicYearsOfFaculty and GetLecturersTeachingClass

---
+++ webserver/src/main/java/api/GetAcademicYearsOfFaculty.java
@@ -0,0 +1,56 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.annotation.WebServlet;
+
+// Others
+import util.DatabaseConnect;
+
+@WebServlet(urlPatterns = "/getAcaFromFaculty")
+public class GetAcademicYearsOfFaculty extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // String requestUrl = req.getRequestURI();
+        // String name = requestUrl.substring("/database/".length());
+
+        System.out.println(req.getParameter("fname"));
+        String fname = req.getParameter("fname");
+
+        String query = "CALL GetAcademicYearsOfFaculty(" + fname + ") ";
+        System.out.println(query);
+
+        try {
+            DatabaseConnect DB = new DatabaseConnect();
+            DB.getConnection();
+            ResultSet res = DB.doQuery(query);
+            List<Map<String, Object>> json_resp = DB.ResultSetToJSON(res);
+
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.addHeader("Access-Control-Allow-Origin", "*"); // remove CORS policy
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(resp.getOutputStream(), json_resp);
+
+            DB.closeConnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+    
