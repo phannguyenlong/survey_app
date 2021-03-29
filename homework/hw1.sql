9ef026f41198cb2ea7e050dc9944b7aae69be318
Long <phannguyenlong0812@gmail.com>
2021-03-29 13:22:34 +0700

    database: fix hw1 with database ver 2

--- homework/hw1.sql
+++ homework/hw1.sql
@@ -7,26 +7,26 @@ CREATE PROCEDURE GetTotalClassesSize(
 BEGIN
 	SELECT SUM(size) AS total_class
 	FROM (
-	SELECT DISTINCT c.code, c.size AS size
+	SELECT DISTINCT c.class_code, c.size AS size
     FROM class c
-	JOIN teaching t ON c.code = t.class_code
-	JOIN lecturer l ON t.lecturer_code = l.code
-	JOIN semester s ON (s.code = c.semester_code)
-	JOIN academic_year a ON (a.code = s.academic_code)
+	JOIN teaching t ON c.class_code = t.class_code
+	JOIN lecturer l ON t.lecturer_code = l.lec_code
+	JOIN semester s ON (s.sem_code = c.semester_code)
+	JOIN academic_year a ON (a.aca_code = s.academic_code)
     JOIN year_fac_pro_mo yfpm ON (yfpm.id_3 = c.id_3)
-	JOIN module m ON (yfpm.module_code = m.code)
+	JOIN module m ON (yfpm.module_code = m.mo_code)
     JOIN year_fac_pro yfp ON (yfp.id_2 = yfpm.id_2 )
-	JOIN program p ON (p.code = yfp.program_code)
+	JOIN program p ON (p.pro_code = yfp.program_code)
     JOIN year_faculty yf ON (yf.id_1 = yfp.id_1)
-	JOIN faculty f ON (f.code = yf.faculty_code)
+	JOIN faculty f ON (f.fa_code = yf.faculty_code)
     WHERE 
-		(a.code = academic_year OR academic_year IS NULL) AND
-		(s.code = semester OR semester IS NULL) AND
-        (f.code = faculty OR faculty IS NULL) AND
-        (p.code = program OR program IS NULL) AND
-        (m.code = module OR module IS NULL) AND
-        (l.code = lecturer OR lecturer IS NULL) AND
-        (c.code = class OR class IS NULL)
+		(a.aca_code = academic_year OR academic_year IS NULL) AND
+		(s.sem_code = semester OR semester IS NULL) AND
+        (f.fa_code = faculty OR faculty IS NULL) AND
+        (p.pro_code = program OR program IS NULL) AND
+        (m.mo_code = module OR module IS NULL) AND
+        (l.lec_code = lecturer OR lecturer IS NULL) AND
+        (c.class_code = class OR class IS NULL)
 ) AS temp;
 END //
 DELIMITER ;
@@ -35,15 +35,15 @@ CALL GetTotalClassesSize("2020-2021",null,null,null,null,23,null);
 
 -- Code for testing 
 select * from class c
-	JOIN teaching t ON c.code = t.class_code
-	JOIN lecturer l ON t.lecturer_code = l.code
-	JOIN semester s ON (s.code = c.semester_code)
-	JOIN academic_year a ON (a.code = s.academic_code)
+	JOIN teaching t ON c.class_code = t.class_code
+	JOIN lecturer l ON t.lecturer_code = l.lec_code
+	JOIN semester s ON (s.sem_code = c.semester_code)
+	JOIN academic_year a ON (a.aca_code = s.academic_code)
     JOIN year_fac_pro_mo yfpm ON (yfpm.id_3 = c.id_3)
-	JOIN module m ON (yfpm.module_code = m.code)
+	JOIN module m ON (yfpm.module_code = m.mo_code)
     JOIN year_fac_pro yfp ON (yfp.id_2 = yfpm.id_2 )
-	JOIN program p ON (p.code = yfp.program_code)
+	JOIN program p ON (p.pro_code = yfp.program_code)
     JOIN year_faculty yf ON (yf.id_1 = yfp.id_1)
-	JOIN faculty f ON (f.code = yf.faculty_code)
-WHERE l.code = 23
-order by c.code;
+	JOIN faculty f ON (f.fa_code = yf.faculty_code)
+-- WHERE l.code = 23
+order by c.class_code;
