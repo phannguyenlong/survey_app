c3586e3bb6c85453c8f2efa6169530973ffe91dd
Hola <Hola@Hola>
2021-03-29 19:43:40 +0700

    server: update getClassServlet

--- webserver/src/main/java/api/questionaire/getClassServlet.java
+++ webserver/src/main/java/api/questionaire/getClassServlet.java
@@ -14,11 +14,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.annotation.WebServlet;
 
-// Others
 import util.DatabaseConnect;
 
 /**
- * write description here
+ * Used for getClass from database 
  */
 @WebServlet(urlPatterns = "/class")
 public class getClassServlet extends HttpServlet {
@@ -26,6 +25,33 @@ public class getClassServlet extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // write code here
+    	String query = "";
+    	String classCode = req.getParameter("class_code");
+    	
+    	if (req.getParameterMap().containsKey("class_code")) {
+    		query = "CALL getClassByCode(" + classCode + ")";
+    	}
+    	else {
+    		query = "CALL getAllClass()";
+    	}
+        System.out.println(query);
+
+        try {
+            DatabaseConnect DB = new DatabaseConnect();
+            DB.getConnection();
+            ResultSet res = DB.doQuery(query);
+            List<Map<String, Object>> json_resp = DB.ResultSetToJSON(res);
+
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.addHeader("Access-Control-Allow-Origin", "*"); // remove CORS policy
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(resp.getOutputStream(), json_resp);
+
+            DB.closeConnect();
+        } catch (Exception ex) {
+            resp.setStatus(500);
+            ex.printStackTrace();
+        }
     }
 }
