f3cf77d08bd50d6392d678426d1a85a388af37b8
Admin <Admin@Admin>
2021-04-01 15:24:38 +0700

    procedure: debug validate procedure

--- init_procedure.sql
+++ init_procedure.sql
@@ -119,24 +119,12 @@ CREATE PROCEDURE insertIntoQuestionaire( class VARCHAR(10), lecturer VARCHAR(10)
                                     answer_19 VARCHAR(2), answer_20 VARCHAR(100)) 
 BEGIN
 	SET @teaching = (SELECT id FROM teaching WHERE teaching.class_code = class AND teaching.lecturer_code = lecturer);
-<<<<<<< HEAD
-
-						INSERT INTO questionaire (teaching_id, answer_1, answer_2, answer_3, answer_4, answer_5,
-=======
 	INSERT INTO questionaire (teaching_id, answer_1, answer_2, answer_3, answer_4, answer_5,
->>>>>>> branch 'main' of https://github.com/phannguyenlong/survey_app
 							answer_6, answer_7, answer_8, answer_9, answer_10, answer_11, answer_12, answer_13,
                             answer_14, answer_15, answer_16, answer_17, answer_18, answer_19, answer_20)
-<<<<<<< HEAD
 						VALUES (@teaching, answer_1, answer_2, answer_3, answer_4, answer_5,
 						answer_6, answer_7, answer_8, answer_9, answer_10, answer_11, answer_12, answer_13,
-						answer_14, answer_15, answer_16, answer_17, answer_18, answer_19, answer_20);            
-=======
-	VALUES (@teaching, answer_1, answer_2, answer_3, answer_4, answer_5,
-			answer_6, answer_7, answer_8, answer_9, answer_10, answer_11, answer_12, answer_13,
-			answer_14, answer_15, answer_16, answer_17, answer_18, answer_19, answer_20);
-            
->>>>>>> branch 'main' of https://github.com/phannguyenlong/survey_app
+						answer_14, answer_15, answer_16, answer_17, answer_18, answer_19, answer_20);
 	select @teaching;
 END //
 DELIMITER ;
