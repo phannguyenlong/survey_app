4a53f1f68658c535e56aa1b0ab07c5441dfd3201
bipowerhcmcity <lmht772000@gmail.com>
2021-04-02 11:38:55 +0700

    Client - Post_Questionnaire

--- webserver/src/main/webapp/hw4/question.js
+++ webserver/src/main/webapp/hw4/question.js
@@ -1,8 +1,17 @@
+let questions;
+let class_code;
+let lecturer_code;
+
 $(document).ready(function() {
 	getClass()
 	
 	selectOption()
 	getQuestion()
+	$("#submit_bnt").click(function(){
+		submitQuestion(questions)
+	})
+	
+	
 	
 });
 	
@@ -28,22 +37,32 @@ function getQuestion() {
 		url: "http://localhost:8080/webserver/question",
 		success: function(data, textStatus, jqXHR) {
 			let json = JSON.parse(JSON.stringify(data))
+			questions = json
             for(let i=0;i<json.length;i++){
             	text = json[i].content.split("-")
 				question = $("<p></p>").text(new String(text[0]))
 				$("#form").append(question)
 				answer = text.slice(1,text.length)
-				console.log(answer)
 				for(let j=0;j<answer.length;j++){
 					$("#form").append(`<label>"${new String(answer[j])}"</label>`)
-					$("#form").append(`<input type="radio" name="${new String(json[i].id)}" value="${j}"/>`)
+					if(i<2){
+						$("#form").append(`<input type="radio" name="${new String(json[i].id)}" value="${answer[j]}"/>`)
+
+					}
+					else{
+						$("#form").append(`<input type="radio" name="${new String(json[i].id)}" value="${j+1}"/>`)
+
+					}
+					
 				}
 				
 //				$("#class_code").append(`<option value="${code}">${code}</option>`)
             }
-			$("#form").append(`<input type="text" />`)
+			//console.log(json[0].content)
 
+			$("#form").append(`<input type="text" id="text_input" />`)			
 		}
+		
 	})
 }
 
@@ -57,6 +76,7 @@ function selectOption(){
 			$("#mod").children("p").remove();
 			$("#lecturer_code").children("option").remove();
        	var code = new String($(this).children("option:selected").val());
+		class_code = $(this).children("option:selected").val()
 		$.ajax({
 			type: 'GET',
 			url:"http://localhost:8080/webserver/class?class_code="+code,
@@ -69,11 +89,74 @@ function selectOption(){
 			$("#mod").append(`<p>"${new String(json[0].mo_code)} - ${json[0].mo_name}"</p>`)
 			
 			for(let i=0;i<json.length;i++){
-            	code = new String(`${json[i].lec_code} - ${json[i].lec_name}`)
-				$("#lecturer_code").append(`<option value="${code}">${code}</option>`)
+            	name = new String(`${json[i].lec_code} - ${json[i].lec_name}`)
+				$("#lecturer_code").append(`<option value="${json[i].lec_code}">${name}</option>`)
             }
-
 		}
     })
 })
+}
+
+function submitQuestion(json){
+	var answers = {};
+
+	lecturer_code = document.getElementById("lecturer_code").value
+	
+	answers.class_code = parseInt(class_code);
+	answers.lecturer_code=parseInt(lecturer_code);
+	 
+	var isPost = true;
+	console.log("Submit",json)
+
+	for(let i=0;i<json.length-1;i++){
+        const rbs = document.querySelectorAll(`input[name="${new String(json[i].id)}"]`);
+        let selectedValue;
+        for (let j=0;j<rbs.length;j++) {
+			//let element = {}
+            if (rbs[j].checked) {
+				selectedValue = rbs[j].value;
+				if(parseInt(selectedValue)==6){
+					selectedValue = "NA";
+				}
+				break;
+            }			
+        }
+		if(selectedValue===undefined){
+			isPost = false;
+		}
+		else{
+			num = i+1;
+			index = "question".concat(num)
+			answers[index] = selectedValue //ES5 from variable to key values
+		}
+    };
+	console.log(answers);
+	
+	if($("#text_input").val !== ""){
+			selectedValue = document.getElementById("text_input").value
+			index = "question".concat(20)
+			answers[index] = selectedValue 
+	}
+	else{
+		isPost= false;
+	}
+
+		
+	
+	if(isPost ===true){
+		$.ajax({
+			type: 'POST',
+			contentType: "application/json",
+			url:"http://localhost:8080/webserver/questionaire/submit",
+			data:JSON.stringify(answers),
+			dataType:"text",
+			success: function(data, textStatus, jqXHR) {
+				alert(data)
+			}
+		})
+	}
+	else{
+		alert("Please answer all the questions :(")
+	}
+	
 }
