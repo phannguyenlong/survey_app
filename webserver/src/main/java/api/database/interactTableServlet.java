f29b39248378fb8d4e362d1b52fb7eaf18689bc3
Long <phannguyenlong0812@gmail.com>
2021-04-09 22:35:55 +0700

    server: more refactor

--- webserver/src/main/java/api/database/interactTableServlet.java
+++ webserver/src/main/java/api/database/interactTableServlet.java
@@ -1,6 +1,5 @@
 package api.database;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -81,7 +80,6 @@ public class interactTableServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         try {
             DatabaseConnect DB = new DatabaseConnect();
             Connection conn = DB.getConnection();
@@ -106,21 +104,6 @@ public class interactTableServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        StringBuffer data = new StringBuffer();
-        String line = null;
-
-        try {
-            BufferedReader reader = req.getReader();
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                data.append(line);
-            }
-        } catch (Exception e) {
-            resp.setStatus(500);
-        }
-
-        System.out.println(data);
-
         try {
             DatabaseConnect DB = new DatabaseConnect();
             Connection conn = DB.getConnection();
@@ -136,21 +119,6 @@ public class interactTableServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        StringBuffer data = new StringBuffer();
-        String line = null;
-
-        try {
-            BufferedReader reader = req.getReader();
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                data.append(line);
-            }
-        } catch (Exception e) {
-            resp.setStatus(500);
-        }
-
-        System.out.println(data);
-
         try {
             DatabaseConnect DB = new DatabaseConnect();
             Connection conn = DB.getConnection();
