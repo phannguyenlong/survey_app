0d58789540e1c76399d512290e008b80113fda24
Long <phannguyenlong0812@gmail.com>
2021-04-17 01:07:36 +0700

    refactor: remove unused file

--- client/test.js
+++
@@ -1,6 +0,0 @@
-fetch("http://localhost:8080/webserver/database").then(res => {
-    if (res.status !== 200) console.log("Error when fetching server");
-    else {
-        res.json().then(data => console.log(data))
-    }
-}).catch (err => console.log(err))
